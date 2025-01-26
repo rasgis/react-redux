474b5a9caba910bfb888aaef42a14bcb6f9ddf2a
Andrey <rasgis@yandex.ru>
2025-01-26 20:29:49 +0300

    first commit

--- src/Components/Field/Field.jsx
+++ src/Components/Field/Field.jsx
@@ -3,59 +3,64 @@ import { actions } from '../../reducer';
 import styles from './Field.module.css';
 
 export default function Field() {
-  const dispatch = useDispatch();
-  const field = useSelector((state) => state.field);
-  const currentPlayer = useSelector((state) => state.currentPlayer);
-  const isGameEnded = useSelector((state) => state.isGameEnded);
+	const dispatch = useDispatch();
+	const field = useSelector((state) => state.field);
+	const currentPlayer = useSelector((state) => state.currentPlayer);
+	const isGameEnded = useSelector((state) => state.isGameEnded);
+	const winningLine = useSelector((state) => state.winningLine); // Получаем победную линию
 
-  const handleCellClick = (index) => {
-    if (field[index] || isGameEnded) return;
+	const handleCellClick = (index) => {
+	  if (field[index] || isGameEnded) return;
 
-    const updatedField = [...field];
-    updatedField[index] = currentPlayer;
+	  const updatedField = [...field];
+	  updatedField[index] = currentPlayer;
 
-    dispatch({ type: actions.SET_FIELD, payload: updatedField });
+	  const winPattern = checkWin(updatedField);
+	  if (winPattern) {
+		dispatch({ type: actions.SET_GAME_ENDED, payload: true });
+		dispatch({ type: actions.SET_WINNING_LINE, payload: winPattern });
+	  } else if (updatedField.every((cell) => cell !== '')) {
+		dispatch({ type: actions.SET_DRAW, payload: true });
+	  } else {
+		dispatch({
+		  type: actions.SET_CURRENT_PLAYER,
+		  payload: currentPlayer === 'X' ? '0' : 'X',
+		});
+	  }
 
-    if (checkWin(updatedField)) {
-      dispatch({ type: actions.SET_GAME_ENDED, payload: true });
-    } else if (updatedField.every((cell) => cell !== '')) {
-      dispatch({ type: actions.SET_DRAW, payload: true });
-    } else {
-      dispatch({
-        type: actions.SET_CURRENT_PLAYER,
-        payload: currentPlayer === 'X' ? '0' : 'X',
-      });
-    }
-  };
+	  dispatch({ type: actions.SET_FIELD, payload: updatedField });
+	};
 
-  const checkWin = (field) => {
-    const winPatterns = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    return winPatterns.some((pattern) =>
-      pattern.every((index) => field[index] === currentPlayer)
-    );
-  };
+	const checkWin = (field) => {
+	  const winPatterns = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	  ];
+	  return winPatterns.find((pattern) =>
+		pattern.every((index) => field[index] === currentPlayer)
+	  );
+	};
 
-  return (
-    <div className={styles.field}>
-      {field.map((cell, index) => (
-        <button
-          key={index}
-          className={styles.cell}
-          onClick={() => handleCellClick(index)}
-        >
-          {cell}
-        </button>
-      ))}
-    </div>
-  );
+	return (
+	  <div className={styles.field}>
+		{field.map((cell, index) => (
+		  <button
+			key={index}
+			className={`${styles.cell} ${
+			  winningLine.includes(index) ? styles.winningCell : ''
+			}`} // Добавляем класс для победных клеток
+			onClick={() => handleCellClick(index)}
+		  >
+			{cell}
+		  </button>
+		))}
+	  </div>
+	);
 }
 
--- src/reducer.js
+++ src/reducer.js
@@ -3,14 +3,16 @@ const initialState = {
     currentPlayer: 'X',
     isGameEnded: false,
     isDraw: false,
+	winningLine: [],
 };
 
 export const actions = {
-    SET_FIELD: 'SET_FIELD',
-    SET_CURRENT_PLAYER: 'SET_CURRENT_PLAYER',
-    RESTART_GAME: 'RESTART_GAME',
-    SET_GAME_ENDED: 'SET_GAME_ENDED',
-    SET_DRAW: 'SET_DRAW',
+	SET_FIELD: 'SET_FIELD',
+	SET_CURRENT_PLAYER: 'SET_CURRENT_PLAYER',
+	RESTART_GAME: 'RESTART_GAME',
+	SET_GAME_ENDED: 'SET_GAME_ENDED',
+	SET_DRAW: 'SET_DRAW',
+	SET_WINNING_LINE: 'SET_WINNING_LINE',
 };
 
 export function reducer(state = initialState, action) {
@@ -25,6 +27,8 @@ export function reducer(state = initialState, action) {
         return { ...state, isGameEnded: action.payload };
       case actions.SET_DRAW:
         return { ...state, isDraw: action.payload };
+	  case actions.SET_WINNING_LINE:
+		return { ...state, winningLine: action.payload };
       default:
         return state;
     }
